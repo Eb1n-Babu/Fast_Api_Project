b3f1edaf505e22a29a762ae76d3159322609637c
Eb1n-Babu <ebin.babu.in@icloud.com>
2025-11-10 14:19:25 +0530

    project updated

---
+++ main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI(title="FastAPI")
+
+book_db = []
+
+class Book(BaseModel):
+    title: str
+    author: str
+
+@app.get("/")
+def home():
+    return {"message": "Hello World"}
+
+@app.get("/books")
+def get_books():
+    return book_db
+
+@app.get("/books/{book_id}")
+def get_book(book_id: int):
+    return book_db[book_id]
+
+@app.post("/books")
+def create_book(book: Book):
+    book_db.append(book)
+    return book

f5c4ae2efa3e34aca039daf7271d19735a0781c9
EBIN BABU <ebin.babu.in@icloud.com>
2026-05-18 18:39:48 +0530

    project updated : files deleted

--- employee.py
+++ employee.py
@@ -1,34 +1,17 @@
 from fastapi import FastAPI
-import mysql.connector
 from decouple import config
+import mysql.connector
 
-emp = mysql.connector.connect(
+app = FastAPI(title="Employee API")
+
+employee_db = mysql.connector.connect(
     host="localhost",
-    user="root",
-    passwd=config('MYSQL_USER'),
-    port=3306,
-    database=config('MYSQL_DATABASE')
+    user=config("USER_NAME"),
+    passwd=config("PASSWORD"),
+    database=config("DATABASE"),
+    port="3306",
 )
 
-app = FastAPI(title="employee")
+if employee_db.is_connected():
+    print("Connected")
 
-@app.get("/employees/")
-def get_employee():
-    cursor = emp.cursor()
-    cursor.execute("SELECT * FROM employees")
-    data = cursor.fetchall()
-    emp.close()
-    return data
-@app.get("/employees/{emp_id}")
-def get_employee(emp_id: int):
-    cursor = emp.cursor()
-    cursor.execute("SELECT * FROM employees WHERE id = %s",(emp_id,))
-    data = cursor.fetchone()
-    emp.close()
-    return data
-@app.post("/employees/")
-def create_employee(first_name, last_name, email, department, salary, hire_date, country, phone, status):
-    cursor = emp.cursor()
-    cursor.execute("INSERT INTO employees(first_name, last_name, email, department, salary, hire_date, country, phone, status)"
-                   "VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)",(first_name, last_name, email, department, salary, hire_date, country, phone, status))
-    emp.commit()
